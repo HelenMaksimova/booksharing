3d3cf9846c06ac106dd3cb2c1d9e9149f4e6afd2
Elena <alnorda@gmail.com>
2021-07-23 00:49:39 +0300

    created CBV for catalog in books app

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from books.views import catalog, book_page
+from books.views import CatalogListView, book_page
 
 app_name = 'books'
 
 urlpatterns = [
-    path('', catalog, name='index'),
-    path('book', book_page, name='book'),
+    path('', CatalogListView.as_view(), name='index'),
+    path('<int:category_id>/', CatalogListView.as_view(), name='category_filter'),
+    path('book/', book_page, name='book'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
+from books.models import Book, Category
+from django.views.generic.list import ListView
 
 
 def index(request):
     return render(request, 'books/index.html')
 
 
-def catalog(request):
-    return render(request, 'books/catalog.html')
-
-
 def book_page(request):
     return render(request, 'books/book-page.html')
+
+
+class CatalogListView(ListView):
+    model = Book
+    template_name = 'books/catalog.html'
+    extra_context = {
+        'title': 'Booksharing - Каталог книг',
+        'categories': Category.objects.all()
+    }
+
+    def get_queryset(self):
+        category_id = self.kwargs.get('category_id')
+        return Book.objects.all() if not category_id else Book.objects.filter(category_id=category_id)
